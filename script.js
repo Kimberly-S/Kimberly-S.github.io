1b876821e8e037d283b03b3f38ad915b20ec6d53
Kimberly Sufangga <kimberlysufangga@gmail.com>
2022-02-10 17:43:12 +1100

    add edits and js for modifying navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+// navbar highlight current page
+const navbar = document.getElementById("navbar");
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const projects = document.getElementById("projects");
+const footer = document.getElementById("footer");
+
+function setNavigation() {
+
+}
