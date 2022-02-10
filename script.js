bc55075830463a937cb650ed4e3eb702ccba2bd0
Kimberly Sufangga <kimberlysufangga@gmail.com>
2022-02-10 20:56:54 +1100

    remove duplicate code

--- script.js
+++ script.js
@@ -1,50 +1,14 @@
 // navbar highlight current page
 
 document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.getElementById("navbar");
     const home = document.getElementById("home");
     const about = document.getElementById("about");
     const projects = document.getElementById("projects");
     const contact = document.getElementById("contact");
 
-    const homeSection = document.getElementById("background");
     const aboutSection = document.getElementById("section2");
     const projectsSection = document.getElementById("section3");
-    const contactSection = document.getElementById("footer");
 
-    window.addEventListener("click", function(event) {
-        if (event.target.id == "home") {
-            about.classList.remove("current");
-            projects.classList.remove("current");
-            contact.classList.remove("current");
-            home.classList.add("current");
-            window.location.href = "#background";
-        }
-
-        if (event.target.id == "about") {
-            about.classList.add("current");
-            projects.classList.remove("current");
-            contact.classList.remove("current");
-            home.classList.remove("current");
-            window.location.href = "#section2";
-        }
-
-        if (event.target.id == "projects") {
-            about.classList.remove("current");
-            projects.classList.add("current");
-            contact.classList.remove("current");
-            home.classList.remove("current");
-            window.location.href = "#section3";
-        }
-
-        if (event.target.id == "contact") {
-            about.classList.remove("current");
-            projects.classList.remove("current");
-            contact.classList.add("current");
-            home.classList.remove("current");
-            window.location.href = "#footer";
-        }
-    });
 
     window.addEventListener("scroll", function(event) {
         let scrollPosition = document.body.scrollTop;
@@ -58,13 +22,13 @@ document.addEventListener('DOMContentLoaded', function() {
             contact.classList.remove("current");
             home.classList.add("current");
         }
-        if (scrollPosition >= aboutPosition) {
+        if (scrollPosition >= aboutPosition - 100) {
             about.classList.add("current");
             projects.classList.remove("current");
             contact.classList.remove("current");
             home.classList.remove("current");
         } 
-        if (scrollPosition >= projectsPosition) {
+        if (scrollPosition >= projectsPosition - 100) {
             about.classList.remove("current");
             projects.classList.add("current");
             contact.classList.remove("current");
